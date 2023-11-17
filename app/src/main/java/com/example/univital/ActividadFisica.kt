c2cbf27727c3a86e74f378ff911efb916b077efa
cesarodc <cesar.olivaresdelc@alumno.buap.mx>
2023-11-17 17:18:48 -0600

    Cambios en actividad fisica y un poco en estadisticas, prueba de ejecucion correcta

--- app/src/main/java/com/example/univital/ActividadFisica.kt
+++ app/src/main/java/com/example/univital/ActividadFisica.kt
@@ -1,11 +1,24 @@
 package com.example.univital
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class ActividadFisica : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_fisica)
+
+        // Buscar el CardView por su ID
+        val cardSleep: CardView = findViewById(R.id.todosLosRegistros)
+
+        // Agregar OnClickListener al CardView
+        cardSleep.setOnClickListener {
+            // Acción a realizar al hacer clic en el CardView (abrir EstadisticasActivity)
+            val intent = Intent(this, Estadisticas::class.java)
+            startActivity(intent)
+        }
+
     }
 }
