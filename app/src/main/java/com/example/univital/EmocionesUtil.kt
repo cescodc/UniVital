49ff4deb5947fa318b40cd073bb9ce2111a14437
zarell <7arell@gmail.com>
2023-11-22 12:36:57 -0600

    Mejora de pantalla de Meditacion, agregar emojis en las emociones para sentirse mas identificado y descripcion mas clara para los botones de la sesion de meditacion.

--- app/src/main/java/com/example/univital/EmocionesUtil.kt
+++ app/src/main/java/com/example/univital/EmocionesUtil.kt
@@ -8,7 +8,7 @@ object EmocionesUtil {
 
     fun configurarSpinner(context: Context, spinner: Spinner) {
         // Define las emociones disponibles
-        val emociones = arrayOf("Selecciona una emoción", "Feliz", "Triste", "Enojado", "Preocupado", "Sorprendido", "Neutral")
+        val emociones = arrayOf("Selecciona una emoción", "\uD83D\uDE0A Feliz ", "\uD83D\uDE22 Triste", "\uD83D\uDE20 Enojado", "\uD83D\uDE1F Preocupado", "\uD83D\uDE32 Sorprendido", "\uD83D\uDE42 Neutral")
 
         // Crea un adaptador para el Spinner
         val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, emociones)
